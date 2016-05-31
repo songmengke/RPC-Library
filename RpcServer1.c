4f665095e87cc458f5f9d80f1d56b514702789d7
zhangqianli_server <songmengke@yeah.net>
2016-05-31 17:58:31 +0800

    success receive data from client use select

--- RpcServer1.c
+++ RpcServer1.c
@@ -4,19 +4,24 @@
 #include<unistd.h>
 #include<sys/types.h>
 #include<sys/socket.h>
+#include<sys/select.h>
 #include<sys/un.h>
 #include<string.h>
 #include<arpa/inet.h>
 int main()
 {
-	int server_fd,client_fd;
+	int server_fd;
+	int client_fd;
 	struct sockaddr_in myaddr;
 	struct sockaddr_in clientaddr;
+	int clientaddr_len=sizeof(clientaddr);
+	int maxfdp;
+	fd_set fds;
+	struct timeval timeout={3,0};
 	myaddr.sin_family=AF_INET;
 	myaddr.sin_addr.s_addr=htonl(INADDR_ANY);
 	myaddr.sin_port=htons(4600);
-	int clientaddr_len=sizeof(clientaddr);
-	char buf[]="songmengke\0";
+	char buf[16];
 	//socket
 	server_fd=socket(AF_INET,SOCK_STREAM,0);
 	if(server_fd==-1)
@@ -37,17 +42,39 @@ int main()
 		exit(1);
 	}
 	printf("listening~\n");
+	client_fd=accept(server_fd,(struct sockaddr *)&clientaddr,&clientaddr_len);
+	while(1)
+	{
+		FD_ZERO(&fds);
+		FD_SET(server_fd,&fds);
+		FD_SET(client_fd,&fds);
+		maxfdp=server_fd>client_fd?server_fd+1:client_fd+1;
+		switch(select(maxfdp,&fds,&fds,NULL,&timeout))
+		{
+			case -1:
+				exit(-1);
+				break;
+			case 0:
+				sleep(2);
+				printf("time out~ \n");
+				break;
+			default:
+				if(FD_ISSET(client_fd,&fds))
+				{
+					sleep(2);
+					recv(client_fd,buf,100,0);
+					printf("receive from client %s\n",buf);
+				}
+		}
+	}
 	//accept
 	
-	client_fd=accept(server_fd,(struct sockaddr *)&clientaddr,&clientaddr_len);
-	if(client_fd==-1)	
-	{
-		perror("accept error");
-		exit(1);
-	}
-	send(client_fd,buf,strlen(buf),0);
-	printf("send to client\n");
-	close(client_fd);
+	//client_fd=accept(server_fd,(struct sockaddr *)&clientaddr,&clientaddr_len);
+	//if(client_fd==-1)	
+	//{
+	//	perror("accept error");
+	//	exit(1);
+	//}
 
 	close(server_fd);
 }
