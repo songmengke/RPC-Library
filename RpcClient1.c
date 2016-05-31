246c9806d1b608551929ee7d3ec970d9c7273f95
songmengke <songmengke>
2016-05-31 09:01:04 +0000

    change client to send message

--- RpcClient1.c
+++ RpcClient1.c
@@ -12,45 +12,22 @@
 int main()
 {
 	int server_fd;
-	int maxfdp;//描述符最大值
 	struct sockaddr_in myaddr;
-	fd_set fds;//使用select方法
-	struct timeval timeout={3,0};
 	myaddr.sin_family=AF_INET;
 	myaddr.sin_addr.s_addr=inet_addr("115.159.44.20");
 	myaddr.sin_port=htons(4600);
-	char buf[16];
+	char buf[]="hello songmengke";
 	
 	server_fd=socket(AF_INET,SOCK_STREAM,0);
-	while(1)
-	{
-		FD_ZERO(&fds);
-		FD_SET(server_fd,&fds);
-		maxfdp=server_fd+1;
-		switch(select(maxfdp,&fds,&fds,NULL,&timeout))
-		{
-			case -1:
-				exit(-1);break;
-			case 0:
-				break;
-			default:
-				if(FD_ISSET(server_fd,&fds))
-				{
-					sleep(1);
-					recv(server_fd,buf,16,0);
-					
-					printf("get message: %s\n",buf);
 
-				}
-		}
-	}
-
-	/*
+	
 	if(connect(server_fd,(struct sockaddr *)&myaddr,sizeof(myaddr))==-1)
 	{
 		perror("connect error");
 		exit(1);
-	}*/
+	}
+	send(server_fd,buf,16,0);
+	printf("send to server %s\n",buf);
 	close(server_fd);
 }
 
