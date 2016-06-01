212b53aa9418eb2a9c29438f410687c44bd5c4e7
zhangqianli_server <songmengke@yeah.net>
2016-06-01 20:04:06 +0800

    try to get function name but failed

--- DataAnalysis.c
+++ DataAnalysis.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<ctype.h>
 #include<string.h>
-int toInt(int *val,char *str)
+int toInt(int *val,char *str,char func[])
 {
 	int tmp=0;
 	int x=0;
@@ -12,6 +12,14 @@ int toInt(int *val,char *str)
 		if(isdigit(str[i]))
 		{
 			tmp=tmp*10+str[i]-'0';
+		}
+		else if(isalpha(str[i]))
+		{
+			func[i]=str[i];
+			if((i>0)&&(!isalnum(str[i+1])))
+			{
+				func[i+1]='\0';
+			}
 		}
 		else
 		{
@@ -29,12 +37,14 @@ int toInt(int *val,char *str)
 }
 int main(int argc ,char **argv)
 {	
+	char func_name[20];
 	int val[120],i;
 	val[0]=0;
-	toInt(val,argv[1]);
+	toInt(val,argv[1],func_name);
 	printf("%d\n",val[0]);
 	for(i=1;i<=val[0];i++)
-		printf("%d",val[i]);
+		printf("%d ",val[i]);
 	puts("");
+	printf("%s\n",func_name);
 	return 0;
 }
