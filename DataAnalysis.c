52434bacb975a9ac1bfba706d89f7ea48a1fc00c
songmengke <songmengke>
2016-06-02 03:24:02 +0000

    modify DataAnalysis.c a little

--- DataAnalysis.c
+++ DataAnalysis.c
@@ -27,14 +27,42 @@ int toInt(int *val,char *str)
 		}
 	}
 }
+int toStr(char val[][100],char *str)
+{
+	int i=0;
+	int len=strlen(str);
+	int tmp=0;
+	int number=0;
+	for(i=0;i<len-4;i++)
+	{
+		if(str[i]=='\\'&&str[i+1]=='r'&&str[i+2]=='\\'&&str[i+3]=='n')
+		{
+			val[number++][tmp]='\0';
+			tmp=0;
+			i+=3;
+		}
+		else
+		{
+			val[number][tmp++]=str[i];
+		}
+	}
+	if(tmp!=0)
+	{
+		val[number++][tmp]='\0';
+	}
+	return number;
+}
 int main(int argc ,char **argv)
 {	
+	if(argc<2) return 0;
 	int val[120],i;
-	val[0]=0;
-	toInt(val,argv[1]);
-	printf("%d\n",val[0]);
-	for(i=1;i<=val[0];i++)
-		printf("%d",val[i]);
-	puts("");
+	int number =0;
+	char mp[120][100];
+	//number=toStr(mp,argv[1]);
+	printf("argument number = %d\n",argc);
+	//for(i=0;i<number;i++)
+	//{
+//		printf("%s \n",mp[i]);
+//	}
 	return 0;
 }
