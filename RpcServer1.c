221690ecd9921c342176791d3748fe1d6a5a6bf9
zhangqianli_server <songmengke@yeah.net>
2016-06-05 00:39:55 +0800

    成功完成数据解析功能函数

--- RpcServer1.c
+++ RpcServer1.c
@@ -8,10 +8,57 @@
 #include<sys/un.h>
 #include<string.h>
 #include<arpa/inet.h>
+int toStr(char val[][100],char *str)
+{
+	int i=0;
+	int len=strlen(str);
+	int tmp=0;
+	int number=0;
+	for(i=0;i<len-2;i++)
+	{
+		if(str[i]=='\r'&&str[i+1]=='\n')
+		{
+			val[number++][tmp]='\0';
+			tmp=0;
+			i++;
+		}
+		else if(str[i]==' ')
+		{
+			val[number++][tmp]='\0';
+			tmp=0;
+		}
+		else
+		{
+			val[number][tmp++]=str[i];
+		}
+	}
+	if(tmp!=0)
+	{
+		val[number++][tmp]='\0';
+	}
+	return number ;
+
+}
+void spile(char *str,char *fun_name,int *argunum,int arguval[])
+{
+	char val[100][100];
+	int number=0;
+	int i;
+	int *pi;
+	number=toStr(val,str);
+	strcpy(fun_name,val[1]);
+	*argunum=number-3;
+	for(i=0,pi=arguval;i<number-3;i++,pi++)//将传递过来的数组首地址赋值给pi，然后依次将参数值保存到数组中
+	{
+		*pi=atoi(val[i+3]);
+	}
+
+}
 int main()
 {
 	int server_fd;//服务器使用到的socket文件描述符
 	int client_fd;//用来接受连接的客户端socket文件描述符
+	int i;
 	struct sockaddr_in myaddr;
 	struct sockaddr_in clientaddr;
 	int clientaddr_len=sizeof(clientaddr);
@@ -22,6 +69,11 @@ int main()
 	myaddr.sin_addr.s_addr=htonl(INADDR_ANY);
 	myaddr.sin_port=htons(4600);//设置为静态端口，后期可更改
 	char buf[100];//用来接收客户端数据的缓存区
+
+	char *function_name;//用于保存接收的函数名
+	int argc;//用于保存传过来的参数个数
+	int *argument,a[100]={0};
+	argument=&a[0];
 	//socket
 	server_fd=socket(AF_INET,SOCK_STREAM,0);//创建服务端socket描述符
 	if(server_fd==-1)
@@ -63,15 +115,24 @@ int main()
 				if(FD_ISSET(client_fd,&fds))
 				{
 					sleep(2);
-					char ExeStr[]="./DataAnalysis ";
 					char ReplyTest[]="calculate result";
 					int rnum=recv(client_fd,buf,50,0);
 					printf("%d\n",strlen(buf));
 					buf[rnum]='\0';
 					printf("receive from client %s\n",buf);
 					printf("Calculating....\n");
-					strcat(ExeStr,buf);
-					system(ExeStr);
+					spile(buf,function_name,&argc,argument);
+
+
+					printf("函数名:%s\n",function_name);
+					printf("参数个数:%d\n",argc);
+					for(i=0;i<argc;i++)
+					{
+						printf("参数值%d---%d \n",i+1,argument[i]);
+					}
+
+
+
 					int snum=send(client_fd,ReplyTest,sizeof(ReplyTest),0);
 					printf("send result %d bytes to client\n ",snum);
 					close(client_fd);
