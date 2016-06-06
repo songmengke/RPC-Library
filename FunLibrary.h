637fa7cc4a79ef78da7fc2fe77d911dc3f50e43f
zhangqianli_server <songmengke@yeah.net>
2016-06-06 10:08:32 +0800

    解决了接收函数名匹配的问题

--- FunLibrary.h
+++ FunLibrary.h
@@ -12,16 +12,16 @@ int sub(int a,int b)
 }
 int MultiFunction(char  func_name[],int argv[],int argc)
 {
-		if(strncmp(func_name,"foo",3)&&argc==3)
+
+		if((strncmp(func_name,"foo",3)==0)&&(argc==3))
 		{
-			printf("MultiFunvtion 接收到函数名%s")
 			return foo(argv[0],argv[1],argv[2]);
 		}
-		else if(strncmp(func_name,"plus",4)&&argc==2)
+		else if((strncmp(func_name,"plus",4)==0)&&argc==2)
 		{
 			return plus(argv[0],argv[1]);
 		}
-		else if(strncmp(func_name,"sub",3)&&argc==2)
+		else if((strncmp(func_name,"sub",3)==0)&&argc==2)
 		{
 			return sub(argv[0],argv[1]);
 		}
